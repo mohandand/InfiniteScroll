5dd12eba5adaf701ff991a723813b4f13b18cafd
mohandand <mohandand@gmail.com>
2022-10-10 17:59:05 -0700

    2 files modified

--- Scroll.js
+++ Scroll.js
@@ -2,16 +2,25 @@ import React, { useEffect, useState } from 'react';
 
 function Scroll() {
   const [imageDump, SetImageDump] = useState([]);
+
   useEffect(() => {
     fetchImages();
-  }, []);
+  }, [imageDump]);
 
   const fetchImages = async () => {
     let data = await fetch('https://dog.ceo/api/breeds/image/random');
     let images = await data.json();
     console.log(images);
+    setTimeout([...images]);
   };
-  return <div>HEllo</div>;
+  let imageData = imageDump.map((img, index) => {
+    return (
+      <div className="imagebox" key={index}>
+        <img src={item.message} width="200px" height="200px" />
+      </div>
+    );
+  });
+  return <div classname="container">{imageData}</div>;
 }
 
 export default Scroll;
